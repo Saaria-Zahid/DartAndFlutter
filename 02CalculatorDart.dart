21b78cc93e027fed645a95cc6296e228cd7c66cb
Saaria-Zahid <saariazahid07@gmail.com>
2024-07-21 00:35:56 +0500

    02

---
+++ 02CalculatorDart.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  int value1 = 15;
+  int value2 = 3;
+  
+  int add = value1 + value2;
+  print("${value1} + ${value2} = ${add}");
+  
+  int sub = value1 - value2;
+  print("${value1} - ${value2} = ${sub}");
+  
+  int multiply = value1 * value2;
+  print("${value1} * ${value2} = ${multiply}");
+  
+  double divide = value1 / value2;
+  print("${value1} / ${value2} = ${divide}");
+}
