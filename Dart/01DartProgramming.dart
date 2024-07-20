774e944bd4445ac712b482c94bd485efc1995a27
Saaria-Zahid <saariazahid07@gmail.com>
2024-07-21 00:35:42 +0500

    01

---
+++ Dart/01DartProgramming.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Saaria Zahid";
+  print("My Name is ${name}");
+  
+  String national = "British";
+  print("I am ${national} Citizen");
+  
+  int age = 15;
+  print("I am ${age} Years Old");
+}
