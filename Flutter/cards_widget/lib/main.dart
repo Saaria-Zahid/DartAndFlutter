226f381c0f4055ab1449b40ff25130b1926f3e0f
Saaria-Zahid <saariazahid07@gmail.com>
2024-09-25 22:14:08 +0500

    Card Widget

--- Flutter/cards_widget/lib/main.dart
+++ Flutter/cards_widget/lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),);
   }
@@ -40,26 +40,30 @@ class _HomeState extends State<Home> {
           elevation: 10,
           shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(10) ) ,
           
-          child: ListTile(
+          child: const ListTile(
             title: Text('My First Card'),
           ),
         ),
          Card(
           elevation: 10,
-          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(20) ,borderSide: BorderSide(color: Colors.green)),
+          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(20) ,borderSide: const BorderSide(color: Colors.green)),
           
-          child: ListTile(
+          child: const ListTile(
             title: Text('My First Card'),
           ),
         ),
-         Card(
-          elevation: 10,
-          shape: CircleBorder(side: BorderSide(width: 1 , color: Colors.green)),
-          
+       
+        Card(
           child: Container(
-            child: 
-             Text('My First Card'),
+            width: 150,
+            height: 150,
+            child: Center(child: Text('Saz')),
           ),
+          shape: CircleBorder(side: BorderSide(width: 3,color: Colors.green)),
+          elevation: 10,
+          margin: EdgeInsets.all(10),
+          surfaceTintColor: Colors.amber,
+
         )
 
         ],
